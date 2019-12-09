5ac4b3d9f63ce3f70e6e0aceac4e4426632cb690
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-12-09 15:42:51 +0300

    Clean up Factory

--- Factory.php
+++ Factory.php
@@ -2,7 +2,7 @@
 
 namespace HubSpot;
 
-use SevenShores\Hubspot\Http\Client;
+use GuzzleHttp\Client;
 
 /**
  * Class Factory.
@@ -15,27 +15,22 @@ class Factory
     /** @var Client */
     private $client;
 
-    /**
-     * C O N S T R U C T O R ( ^_^)y.
-     *
-     * @param array  $config        An array of configurations. You need at least the 'key'.
-     * @param Client $client
-     * @param array  $clientOptions options to be send with each request
-     * @param bool   $wrapResponse  wrap request response in own Response object
-     */
-    public function __construct($config = [], $client = null, $clientOptions = [], $wrapResponse = true)
+    public function __construct()
     {
-        $this->client = $client ?: new Client($config, null, $clientOptions, $wrapResponse);
+        $client = new Client([
+            'base_uri' => 'https://api.hubapi.com/crm/v3/objects',
+        ]);
+
+        $this->client = $client;
     }
 
     /**
      * Return an instance of a Resource based on the method called.
      *
      * @param string $name
-     * @param array  $arguments
-     * @param mixed  $args
+     * @param mixed $args
      *
-     * @return \SevenShores\Hubspot\Resources\Resource
+     * @return mixed
      */
     public function __call($name, $args)
     {
@@ -53,47 +48,10 @@ class Factory
     }
 
     /**
-     * Create an instance of the service with an API key.
-     *
-     * @param string $api_key       hubspot API key
-     * @param Client $client        an Http client
-     * @param array  $clientOptions options to be send with each request
-     * @param bool   $wrapResponse  wrap request response in own Response object
-     *
      * @return static
      */
-    public static function create($api_key = null, $client = null, $clientOptions = [], $wrapResponse = true)
+    public static function create()
     {
-        return new static(['key' => $api_key], $client, $clientOptions, $wrapResponse);
-    }
-
-    /**
-     * Create an instance of the service with an OAuth token.
-     *
-     * @param string $token         hubspot oauth access token
-     * @param Client $client        an Http client
-     * @param array  $clientOptions options to be send with each request
-     * @param bool   $wrapResponse  wrap request response in own Response object
-     *
-     * @return static
-     */
-    public static function createWithToken($token, $client = null, $clientOptions = [], $wrapResponse = true)
-    {
-        return new static(['key' => $token, 'oauth' => true], $client, $clientOptions, $wrapResponse);
-    }
-
-    /**
-     * Create an instance of the service with an OAuth2 token.
-     *
-     * @param string $token         hubspot OAuth2 access token
-     * @param Client $client        an Http client
-     * @param array  $clientOptions options to be send with each request
-     * @param bool   $wrapResponse  wrap request response in own Response object
-     *
-     * @return static
-     */
-    public static function createWithOAuth2Token($token, $client = null, $clientOptions = [], $wrapResponse = true)
-    {
-        return new static(['key' => $token, 'oauth2' => true], $client, $clientOptions, $wrapResponse);
+        return new static();
     }
 }
