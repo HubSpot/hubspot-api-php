898f0670095b14d5278b6d0287e1f5606bd55427
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-12-26 18:04:44 +0300

    Adopt contacts for new SDK namespaces

--- sample-apps/contacts-app/src/actions/contacts/new.php
+++ sample-apps/contacts-app/src/actions/contacts/new.php
@@ -8,7 +8,7 @@ $hubSpot = \Helpers\HubspotClientHelper::createFactory();
 if (isset($_POST['email'])) {
     $contactInput = new ContactInput();
     $contactInput->setProperties($_POST);
-    $contact = $hubSpot->objects()->createNativeObjectsApi()->postcrmv3objectscontacts($contactInput);
+    $contact = $hubSpot->crm()->objects()->createNativeObjectsApi()->postcrmv3objectscontacts($contactInput);
 
     header('Location: /contacts/show.php?id='.$contact['id']);
     exit();
