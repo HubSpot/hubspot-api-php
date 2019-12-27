4648c3fc9c058f0f2f239b5479357d1962e023d2
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-12-27 15:18:30 +0300

    Fix OAuth imports

--- lib/Discovery/OAuth/Discovery.php
+++ lib/Discovery/OAuth/Discovery.php
@@ -2,15 +2,15 @@
 
 namespace HubSpot\Discovery\OAuth;
 
-use HubSpot\Client\Crm\OAuth\Api\AccessTokensApi;
-use HubSpot\Client\Crm\OAuth\Api\RefreshTokensApi;
-use HubSpot\Client\Crm\OAuth\Api\TokensApi;
+use HubSpot\Client\OAuth\Api\AccessTokensApi;
+use HubSpot\Client\OAuth\Api\RefreshTokensApi;
+use HubSpot\Client\OAuth\Api\TokensApi;
 use HubSpot\Discovery\DiscoveryBase;
 
 /**
- * @method AccessTokensApi accessTokensApi
- * @method RefreshTokensApi refreshTokensApi
- * @method TokensApi tokensApi
+ * @method AccessTokensApi  accessTokensApi()
+ * @method RefreshTokensApi refreshTokensApi()
+ * @method TokensApi        tokensApi()
  */
 class Discovery extends DiscoveryBase
 {
