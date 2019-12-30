822fb842808e35353f3196478287dc9d6e9145fc
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-12-30 15:02:13 +0300

    Fix naming of create contact

--- sample-apps/contacts-app/src/actions/contacts/new.php
+++ sample-apps/contacts-app/src/actions/contacts/new.php
@@ -8,7 +8,7 @@ $hubSpot = \Helpers\HubspotClientHelper::createFactory();
 if (isset($_POST['email'])) {
     $contactInput = new ContactInput();
     $contactInput->setProperties($_POST);
-    $contact = $hubSpot->crm()->objects()->createNativeObjectsApi()->postcrmv3objectscontacts($contactInput);
+    $contact = $hubSpot->crm()->objects()->createNativeObjectsApi()->createContact($contactInput);
 
     header('Location: /contacts/show.php?id='.$contact['id']);
     exit();
