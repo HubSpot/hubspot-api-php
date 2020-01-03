a89cbcf9d22f75da9c3118686b3ff633fc592706
ksvirkou <ksvirkou@hubspot.com>
2020-01-03 23:43:09 +0300

    delete company + search

---
+++ sample-apps/companies-app/src/actions/companies/delete.php
@@ -0,0 +1,13 @@
+<?php
+
+use Helpers\HubspotClientHelper;
+use HubSpot\Crm\ObjectType;
+
+if (!isset($_GET['id'])) {
+    throw new \Exception('Contact id is not specified');
+}
+HubspotClientHelper::createFactory()->crm()->objects()->basicApi()
+        ->archive(ObjectType::COMPANY, $_GET['id']);
+
+header('Location: /companies/list.php');
+exit();
