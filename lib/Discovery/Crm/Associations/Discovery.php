6197e34a4d732b5c1ccaeebf856e6a761c5089c7
ksvirkou <ksvirkou@hubspot.com>
2020-01-20 13:33:13 +0300

    added Associations

---
+++ lib/Discovery/Crm/Associations/Discovery.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HubSpot\Discovery\Crm\Associations;
+
+use HubSpot\Client\Crm\Associations\Api\BatchApi;
+use HubSpot\Discovery\DiscoveryBase;
+
+/**
+ * @method BatchApi        batchApi()
+ */
+class Discovery extends DiscoveryBase
+{
+}
--- lib/Discovery/Crm/Discovery.php
+++ lib/Discovery/Crm/Discovery.php
@@ -5,11 +5,12 @@ namespace HubSpot\Discovery\Crm;
 use HubSpot\Discovery\DiscoveryBase;
 
 /**
- * @method Contacts\Discovery   contacts()
- * @method Companies\Discovery  companies()
- * @method Objects\Discovery    objects()
- * @method Owners\Discovery     owners()
- * @method Properties\Discovery properties()
+ * @method Associations\Discovery   associations()
+ * @method Contacts\Discovery       contacts()
+ * @method Companies\Discovery      companies()
+ * @method Objects\Discovery        objects()
+ * @method Owners\Discovery         owners()
+ * @method Properties\Discovery     properties()
  */
 class Discovery extends DiscoveryBase
 {
