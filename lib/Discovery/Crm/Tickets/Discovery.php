178433b6b728285bd2cd57df519b8bbdd4e02d12
ksvirkou <ksvirkou@hubspot.com>
2020-01-21 11:06:10 +0300

    add Tickets

---
+++ lib/Discovery/Crm/Tickets/Discovery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace HubSpot\Discovery\Crm\Tickets;
+
+use HubSpot\Client\Crm\Tickets\Api\AssociationsApi;
+use HubSpot\Client\Crm\Tickets\Api\BasicApi;
+use HubSpot\Client\Crm\Tickets\Api\BatchApi;
+use HubSpot\Client\Crm\Tickets\Api\SearchApi;
+use HubSpot\Discovery\DiscoveryBase;
+
+/**
+ * @method AssociationsApi associationsApi()
+ * @method BatchApi        batchApi()
+ * @method BasicApi        basicApi()
+ * @method SearchApi       searchApi()
+ */
+class Discovery extends DiscoveryBase
+{
+}
---
+++ tests/spec/Discovery/Crm/Tickets/DiscoverySpec.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace spec\HubSpot\Discovery\Crm\Tickets;
+
+use GuzzleHttp\Client;
+use HubSpot\Client\Crm\Tickets\Api\AssociationsApi;
+use HubSpot\Client\Crm\Tickets\Api\BasicApi;
+use HubSpot\Client\Crm\Tickets\Api\BatchApi;
+use HubSpot\Client\Crm\Tickets\Api\SearchApi;
+use HubSpot\Config;
+use PhpSpec\ObjectBehavior;
+
+class DiscoverySpec extends ObjectBehavior
+{
+    public function let(Client $client, Config $config)
+    {
+        $this->beConstructedWith($client, $config);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(\HubSpot\Discovery\Crm\Tickets\Discovery::class);
+    }
+
+    public function it_creates_clients()
+    {
+        $this->basicApi()->shouldHaveType(BasicApi::class);
+        $this->batchApi()->shouldHaveType(BatchApi::class);
+        $this->associationsApi()->shouldHaveType(AssociationsApi::class);
+        $this->searchApi()->shouldHaveType(SearchApi::class);
+    }
+}
