9f5076c47137b105558fed78171e7a460420eaf9
Aleh Tanasiuk <atanasiuk@hubspot.com>
2020-01-22 14:34:41 +0300

    Update oauth app title

--- sample-apps/oauth-app/src/views/_partials/header.php
+++ sample-apps/oauth-app/src/views/_partials/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>HubSpot PHP sample contacts app</title>
+  <title>HubSpot PHP sample oauth app</title>
   <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css">
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css">
