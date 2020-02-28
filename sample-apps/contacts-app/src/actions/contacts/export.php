5091c8161fe1acd08997073aaa9b6414921da0d9
ksvirkou <ksvirkou@hubspot.com>
2020-02-28 18:14:37 +0300

    added get all + rewrite export

--- sample-apps/contacts-app/src/actions/contacts/export.php
+++ sample-apps/contacts-app/src/actions/contacts/export.php
@@ -1,58 +1,17 @@
 <?php
 
-use HubSpot\Client\Crm\Objects\Model\CollectionResponseSimplePublicObject;
-use HubSpot\Client\Crm\Objects\Model\SimplePublicObject;
+use Helpers\HubspotClientHelper;
 
-function get_contacts_for_export($maxPages = 10)
-{
-    $hubSpot = Helpers\HubspotClientHelper::createFactory();
-    $contacts = [];
-    $after = null;
-    for ($pageNum = 0; $pageNum < $maxPages; ++$pageNum) {
-        // https://developers.hubspot.com/docs-beta/crm/contacts
-        /** @var CollectionResponseSimplePublicObject $contactsPage */
-        $contactsPage = $hubSpot->crm()->contacts()->basicApi()->getPage(10, $after);
-        $contacts = [
-            ...$contacts,
-            ...$contactsPage->getResults(),
-        ];
-        $next = $contactsPage->getPaging()->getNext();
-        if (null === $next) {
-            break;
-        }
-        $after = $next->getAfter();
-    }
-
-    return $contacts;
-}
-
-function generate_csv_rows($contacts, $properties)
-{
-    $headerRow = [
-        ...$properties,
-    ];
-    $rows = [$headerRow];
-    /** @var SimplePublicObject $contact */
-    foreach ($contacts as $contact) {
-        $row = [];
-        foreach ($properties as $property) {
-            $row[] = $contact->getProperties()[$property];
-        }
-        $rows[] = $row;
-    }
-
-    return $rows;
-}
-
-$contacts = get_contacts_for_export();
-
-$propertiesToExport = ['email', 'firstname', 'lastname'];
-$csvRows = generate_csv_rows($contacts, $propertiesToExport);
+$contacts = HubspotClientHelper::createFactory()->crm()->contacts()->getAll();
 
 header('Content-type: application/csv');
 header('Content-Disposition: attachment; filename=contacts.csv');
-$fp = fopen('php://output', 'w');
-foreach ($csvRows as $row) {
-    fputcsv($fp, $row);
+$file = fopen('php://output', 'w');
+
+fputcsv($file, ['Email', 'Firstname', 'Lastname']);
+
+foreach ($contacts as $contact) {
+    fputcsv($file, array_values($contact->getProperties()));
 }
-fclose($fp);
+
+fclose($file);
