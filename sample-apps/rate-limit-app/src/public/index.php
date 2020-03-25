bfff7394c7ced6ad6c3927a7ded22b5a75f4634f
ksvirkou <ksvirkou@hubspot.com>
2020-03-25 12:20:29 +0300

    fix oauth redirect

--- sample-apps/rate-limit-app/src/public/index.php
+++ sample-apps/rate-limit-app/src/public/index.php
@@ -20,6 +20,7 @@ try {
     if (in_array($uri, $protectedRoutes)) {
         if (!OAuth2Helper::isAuthenticated()) {
             header('Location: /oauth/login.php');
+            exit();
         }
     }
 
