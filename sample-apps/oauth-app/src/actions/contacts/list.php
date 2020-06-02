861f40439ca4d5acc8018e8e39c1a2ec1b19d9f2
ksvirkou <ksvirkou@hubspot.com>
2020-06-02 20:19:30 +0300

    rewrite title

--- sample-apps/oauth-app/src/actions/contacts/list.php
+++ sample-apps/oauth-app/src/actions/contacts/list.php
@@ -5,7 +5,7 @@ use HubSpot\Client\Crm\Contacts\Model\CollectionResponseSimplePublicObject;
 
 $hubSpot = HubspotClientHelper::createFactory();
 
-https://developers.hubspot.com/docs/api/crm/contacts
+//https://developers.hubspot.com/docs/api/crm/contacts
 /** @var CollectionResponseSimplePublicObject $contactsPage */
 $contactsPage = $hubSpot->crm()->contacts()->basicApi()->getPage();
 
