4e1303ef56d057604a93a70b6fff09d40193dc3b
ksvirkou <ksvirkou@hubspot.com>
2020-06-16 14:49:29 +0300

    codegen

--- codegen/Crm/Quotes/Api/SearchApi.php
+++ codegen/Crm/Quotes/Api/SearchApi.php
@@ -380,10 +380,6 @@ class SearchApi
         if ($apiKey !== null) {
             $queryParams['hapikey'] = $apiKey;
         }
-        // this endpoint requires OAuth (access token)
-        if ($this->config->getAccessToken() !== null) {
-            $headers['Authorization'] = 'Bearer ' . $this->config->getAccessToken();
-        }
 
         $defaultHeaders = [];
         if ($this->config->getUserAgent()) {
