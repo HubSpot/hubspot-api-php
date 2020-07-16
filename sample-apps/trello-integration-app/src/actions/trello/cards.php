77941e444afee3a1fc3b2691019f8ce7b889216a
ksvirkou <ksvirkou@hubspot.com>
2020-07-16 12:36:12 +0300

    show card

--- sample-apps/trello-integration-app/src/actions/trello/cards.php
+++ sample-apps/trello-integration-app/src/actions/trello/cards.php
@@ -1,6 +1,17 @@
 <?php
 
 use Formatters\CardsFormatter;
+use Repositories\AssociationRepository;
+use Helpers\TrelloApi;
 
 header('Content-Type: application/json');
-echo json_encode(CardsFormatter::cardExtensionDataResponse());
+if (isset($_GET['hs_object_id'])) {
+    $associatedDeal = AssociationRepository::findOneByDealId($_GET['hs_object_id']);
+    
+    $card = TrelloApi::getCard($associatedDeal['card_id']);
+    echo json_encode(CardsFormatter::cardExtensionDataResponse($associatedDeal, $card));
+} else {
+    
+    echo json_encode(CardsFormatter::cardExtensionDataResponse());
+}
+
--- sample-apps/trello-integration-app/src/views/trello/search-frame.php
+++ sample-apps/trello-integration-app/src/views/trello/search-frame.php
@@ -13,50 +13,50 @@ use Helpers\UrlHelper;
   <script type="application/javascript" src="<?php echo UrlHelper::generateServerUri(); ?>/js/typeahead.bundle.min.js"></script>
 </head>
 <body>
-<form method="POST">
-  <fieldset>
-    <div class="row">
-      <div class="column">
-          <h4>Search for Trello card to attach to the following HubSpot deal: <b><?php echo $dealName; ?></b></h4>
-        <input class="typeahead" type="text" placeholder="Search..">
-        <input type="hidden" name="card_id" id="cardId" />
-      </div>
-    </div>
-    <input type="submit" value="Associate"/>
-  </fieldset>
-</form>
+    <form method="POST">
+        <fieldset>
+            <div class="row">
+                <div class="column">
+                    <h4>Search for Trello card to attach to the following HubSpot deal: <b><?php echo $dealName; ?></b></h4>
+                    <input class="typeahead" type="text" placeholder="Search..">
+                    <input type="hidden" name="card_id" id="cardId" />
+                </div>
+            </div>
+            <input type="submit" value="Associate"/>
+        </fieldset>
+    </form>
 
-<script>
-  $(document).ready(() => {
-    const trelloCardsSource = new Bloodhound({
-      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-      queryTokenizer: Bloodhound.tokenizers.whitespace,
-      remote: {
-        url: "<?php echo UrlHelper::getUrl('/trello/search'); ?>?q=%QUERY",
-        wildcard: '%QUERY'
-      }
-    });
-    // https://twitter.github.io/typeahead.js/examples/
-    $('.typeahead').typeahead({
-      hint: true,
-      highlight: true,
-      minLength: 1
-    }, {
-      name: 'trello-cards',
-      display: 'name',
-      source: trelloCardsSource,
-      templates: {
-        empty: [
-          '<div class="empty-message">',
-            'Unable to find any Trello card that matches the current query',
-          '</div>'
-        ].join('\n'),
-      },
-    }).on("typeahead:selected typeahead:autocompleted", (e, card) => {
-      $("#cardId").val(card.id);
-    });
-  });
-</script>
+    <script>
+      $(document).ready(() => {
+        const trelloCardsSource = new Bloodhound({
+          datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+          queryTokenizer: Bloodhound.tokenizers.whitespace,
+          remote: {
+            url: "<?php echo UrlHelper::getUrl('/trello/search'); ?>?q=%QUERY",
+            wildcard: '%QUERY'
+          }
+        });
+        // https://twitter.github.io/typeahead.js/examples/
+        $('.typeahead').typeahead({
+          hint: true,
+          highlight: true,
+          minLength: 1
+        }, {
+          name: 'trello-cards',
+          display: 'name',
+          source: trelloCardsSource,
+          templates: {
+            empty: [
+              '<div class="empty-message">',
+                'Unable to find any Trello card that matches the current query',
+              '</div>'
+            ].join('\n'),
+          },
+        }).on("typeahead:selected typeahead:autocompleted", (e, card) => {
+          $("#cardId").val(card.id);
+        });
+      });
+    </script>
 </body>
 
 </html>
