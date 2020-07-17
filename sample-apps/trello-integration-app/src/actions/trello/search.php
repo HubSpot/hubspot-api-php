944ab8704e1e92167bd15e7ac3f6af19ec624ffd
ksvirkou <ksvirkou@hubspot.com>
2020-07-17 14:15:18 +0300

    move search to helper

--- sample-apps/trello-integration-app/src/actions/trello/search.php
+++ sample-apps/trello-integration-app/src/actions/trello/search.php
@@ -1,33 +1,16 @@
 <?php
 
-use GuzzleHttp\Client;
-use Repositories\TokensRepository;
+use Helpers\TrelloApi;
 
 if (!isset($_GET['q'])) {
     throw new Exception('Invalid Query');
 }
 
-$headers = [
-    'Accept' => 'application/json',
-];
-
-$query = [
-    'key' => $_ENV['TRELLO_API_KEY'],
-    'token' => TokensRepository::getToken(TokensRepository::TRELLO_TOKEN),
-    'query' => $_GET['q'],
-];
-
-$client = new Client();
-$response = $client->get(
-    'https://api.trello.com/1/search?'.http_build_query($query),
-    ['headers' => $headers]
-);
-
-$data = json_decode($response->getBody()->getContents());
+$response = TrelloApi::search($_GET['q']);
 
 $result = [];
-if (!empty($data->cards)) {
-    foreach ($data->cards as $card) {
+if (!empty($response->cards)) {
+    foreach ($response->cards as $card) {
         $result[] = [
             'id' => $card->id,
             'name' => $card->name,
