6fa9c5a5576fdc6432e220d5c9113b5802ca5243
ksvirkou <ksvirkou@hubspot.com>
2020-08-21 15:31:04 +0300

    fix map link

--- sample-apps/trello-integration-app/src/views/readme.php
+++ sample-apps/trello-integration-app/src/views/readme.php
@@ -5,7 +5,7 @@
         <h3>Proceed with the following steps to see trello integration in action:</h3>
         <ol>
             <li>Associate deal with Trello card using <a href="https://app.hubspot.com/">HubSpot</a> interface</li>
-            <li>Map Trello board lists with HubSpot pipeline stages on <a href="{{ url_for('mappings.home') }}">mappings</a> page</li>
+            <li>Map Trello board lists with HubSpot pipeline stages on <a href="/mappings/boards">mappings</a> page</li>
             <li>Move associated trello card to any other mapped earlier board list and check how this affects HubSpot deal stage</li>
         </ol>
     </div>
