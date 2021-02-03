27694c23482bf2f79395c564d7555419a0efc83c
ksvirkou <ksvirkou@hubspot.com>
2021-02-03 17:25:35 +0300

    add Discoveries

--- lib/Discovery/Discovery.php
+++ lib/Discovery/Discovery.php
@@ -3,10 +3,12 @@
 namespace HubSpot\Discovery;
 
 /**
- * @method Cms\Discovery      cms()
- * @method Crm\Discovery      crm()
- * @method Auth\Discovery     auth()
- * @method Webhooks\Discovery webhooks()
+ * @method Auth\Discovery           auth()
+ * @method Automation\Discovery     automation()
+ * @method Cms\Discovery            cms()
+ * @method Crm\Discovery            crm()
+ * @method Marketing\Discovery      marketing()
+ * @method Webhooks\Discovery       webhooks()
  */
 class Discovery extends DiscoveryBase
 {
