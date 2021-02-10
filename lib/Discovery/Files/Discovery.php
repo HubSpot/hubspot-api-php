03ce1ef4c65a5050a41f49c9db57a93902095cc1
ksvirkou <ksvirkou@hubspot.com>
2021-02-10 22:08:28 +0300

    cs fix

--- lib/Discovery/Files/Discovery.php
+++ lib/Discovery/Files/Discovery.php
@@ -2,13 +2,13 @@
 
 namespace HubSpot\Discovery\Files;
 
-use HubSpot\Client\Files\Api\FilesApi;
-use HubSpot\Client\Files\Api\FoldersApi;
+use HubSpot\Client\Files\Files\Api\FilesApi;
+use HubSpot\Client\Files\Files\Api\FoldersApi;
 use HubSpot\Discovery\DiscoveryBase;
 
 /**
- * @method FilesApi     filesApi()
- * @method FoldersApi   foldersApi()
+ * @method FilesApi   filesApi()
+ * @method FoldersApi foldersApi()
  */
 class Discovery extends DiscoveryBase
 {
