81fc84e4fb06662f466561a9ad18ef3c19caaedf
ksvirkou <ksvirkou@hubspot.com>
2022-03-21 17:19:12 +0300

    add Events to  Marketing

--- lib/Discovery/Marketing/Discovery.php
+++ lib/Discovery/Marketing/Discovery.php
@@ -5,6 +5,7 @@ namespace HubSpot\Discovery\Marketing;
 use HubSpot\Discovery\DiscoveryBase;
 
 /**
+ * @method Events\Discovery        events()
  * @method Transactional\Discovery transactional()
  */
 class Discovery extends DiscoveryBase
