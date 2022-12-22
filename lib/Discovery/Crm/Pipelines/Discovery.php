de934ea276c9f24ec0b9b4e9137eed9f86232234
ksvirkou-hubspot <ksvirkou@hubspot.com>
2022-12-22 17:09:45 +0300

    fix crm()->pipelines() APIs list

--- lib/Discovery/Crm/Pipelines/Discovery.php
+++ lib/Discovery/Crm/Pipelines/Discovery.php
@@ -2,13 +2,17 @@
 
 namespace HubSpot\Discovery\Crm\Pipelines;
 
+use HubSpot\Client\Crm\Pipelines\Api\PipelineAuditsApi;
 use HubSpot\Client\Crm\Pipelines\Api\PipelinesApi;
+use HubSpot\Client\Crm\Pipelines\Api\PipelineStageAuditsApi;
 use HubSpot\Client\Crm\Pipelines\Api\PipelineStagesApi;
 use HubSpot\Discovery\DiscoveryBase;
 
 /**
- * @method PipelineStagesApi pipelineStagesApi()
- * @method PipelinesApi      pipelinesApi()
+ * @method PipelineAuditsApi      pipelineAuditsApi()
+ * @method PipelinesApi           pipelinesApi()
+ * @method PipelineStageAuditsApi pipelineStageAuditsApi()
+ * @method PipelineStagesApi      pipelineStagesApi()
  */
 class Discovery extends DiscoveryBase
 {
