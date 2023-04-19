d10fa6a8c203602e681dc62b5077b3227b36766d
ksvirkou-hubspot <ksvirkou-hubspot@users.noreply.github.com>
2023-04-19 14:04:07 +0000

    Apply php-cs-fixer changes

--- lib/Http/Request.php
+++ lib/Http/Request.php
@@ -113,7 +113,7 @@ class Request
         $urlStr .= $this->options['path'] ?? '';
 
         if (array_key_exists('qs', $this->options) && !empty($this->options['qs'])) {
-            $urlStr .= '?' . http_build_query($this->options['qs'], '', '&', PHP_QUERY_RFC3986);
+            $urlStr .= '?'.http_build_query($this->options['qs'], '', '&', PHP_QUERY_RFC3986);
         }
 
         return $urlStr;
