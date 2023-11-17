fc1b8a493b7ad25aeedd4eef8bfc5a54eef5b3dd
ksvirkou-hubspot <ksvirkou@hubspot.com>
2023-11-17 12:12:15 +0300

    Discovery for marketing forms

---
+++ lib/Discovery/Marketing/Forms/Discovery.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace HubSpot\Discovery\Marketing\Forms;
+
+use HubSpot\Client\Marketing\Forms\Api\FormsApi;
+use HubSpot\Discovery\DiscoveryBase;
+
+/**
+ * @method FormsApi formsApi()
+ */
+class Discovery extends DiscoveryBase
+{
+}
