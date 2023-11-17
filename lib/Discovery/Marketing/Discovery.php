d21cfaa0c00690955a2d994888417867c113afa6
ksvirkou-hubspot <ksvirkou@hubspot.com>
2023-11-17 12:49:20 +0300

    cs fix

--- lib/Discovery/Marketing/Discovery.php
+++ lib/Discovery/Marketing/Discovery.php
@@ -5,9 +5,9 @@ namespace HubSpot\Discovery\Marketing;
 use HubSpot\Discovery\DiscoveryBase;
 
 /**
- * @method Events\Discovery         events()
- * @method Forms\Discovery          forms()
- * @method Transactional\Discovery  transactional()
+ * @method Events\Discovery        events()
+ * @method Forms\Discovery         forms()
+ * @method Transactional\Discovery transactional()
  */
 class Discovery extends DiscoveryBase
 {
