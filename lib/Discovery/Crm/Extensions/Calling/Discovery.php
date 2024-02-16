340a0bba0e39ba20628f1abd6f9bc5b804651450
ksvirkou-hubspot <ksvirkou@hubspot.com>
2024-02-16 12:45:38 +0300

    Added recordingSettingsApi to CRM extensions calling discovery

--- lib/Discovery/Crm/Extensions/Calling/Discovery.php
+++ lib/Discovery/Crm/Extensions/Calling/Discovery.php
@@ -2,10 +2,12 @@
 
 namespace HubSpot\Discovery\Crm\Extensions\Calling;
 
+use HubSpot\Client\Crm\Extensions\Calling\Api\RecordingSettingsApi;
 use HubSpot\Client\Crm\Extensions\Calling\Api\SettingsApi;
 use HubSpot\Discovery\DiscoveryBase;
 
 /**
- * @method SettingsApi settingsApi()
+ * @method RecordingSettingsApi recordingSettingsApi()
+ * @method SettingsApi          settingsApi()
  */
 class Discovery extends DiscoveryBase {}
