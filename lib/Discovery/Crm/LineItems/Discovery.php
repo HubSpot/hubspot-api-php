f8c484b1f9533d767d6b060d5494f1fd7de4bb47
ksvirkou-hubspot <ksvirkou@hubspot.com>
2024-02-27 12:57:17 +0300

    CRM Objects: Update discoveries.

--- lib/Discovery/Crm/LineItems/Discovery.php
+++ lib/Discovery/Crm/LineItems/Discovery.php
@@ -4,14 +4,25 @@ namespace HubSpot\Discovery\Crm\LineItems;
 
 use HubSpot\Client\Crm\LineItems\Api\BasicApi;
 use HubSpot\Client\Crm\LineItems\Api\BatchApi;
+use HubSpot\Client\Crm\LineItems\Api\GDPRApi;
 use HubSpot\Client\Crm\LineItems\Api\PublicObjectApi;
 use HubSpot\Client\Crm\LineItems\Api\SearchApi;
+use HubSpot\Client\Crm\LineItems\Configuration;
 use HubSpot\Discovery\Crm\ObjectDiscovery;
 
 /**
  * @method BasicApi        basicApi()
  * @method BatchApi        batchApi()
+ * @method GDPRApi         gdprApi()
  * @method PublicObjectApi publicObjectApi()
  * @method SearchApi       searchApi()
  */
-class Discovery extends ObjectDiscovery {}
+class Discovery extends ObjectDiscovery
+{
+    public function gdprApi()
+    {
+        $config = $this->config->convertToClientConfig(Configuration::class);
+
+        return new GDPRApi($this->client, $config);
+    }
+}
--- lib/Discovery/Crm/Products/Discovery.php
+++ lib/Discovery/Crm/Products/Discovery.php
@@ -4,14 +4,25 @@ namespace HubSpot\Discovery\Crm\Products;
 
 use HubSpot\Client\Crm\Products\Api\BasicApi;
 use HubSpot\Client\Crm\Products\Api\BatchApi;
+use HubSpot\Client\Crm\Products\Api\GDPRApi;
 use HubSpot\Client\Crm\Products\Api\PublicObjectApi;
 use HubSpot\Client\Crm\Products\Api\SearchApi;
+use HubSpot\Client\Crm\Products\Configuration;
 use HubSpot\Discovery\Crm\ObjectDiscovery;
 
 /**
  * @method BasicApi        basicApi()
  * @method BatchApi        batchApi()
+ * @method GDPRApi         gdprApi()
  * @method PublicObjectApi publicObjectApi()
  * @method SearchApi       searchApi()
  */
-class Discovery extends ObjectDiscovery {}
+class Discovery extends ObjectDiscovery
+{
+    public function gdprApi()
+    {
+        $config = $this->config->convertToClientConfig(Configuration::class);
+
+        return new GDPRApi($this->client, $config);
+    }
+}
