94b9297dd9d4a0ce70ea14763302a83944e252ef
ksvirkou-hubspot <ksvirkou@hubspot.com>
2024-02-27 12:26:38 +0300

    CRM Companies and Deals: Fix discoveries.

--- lib/Discovery/Crm/Deals/Discovery.php
+++ lib/Discovery/Crm/Deals/Discovery.php
@@ -7,6 +7,7 @@ use HubSpot\Client\Crm\Deals\Api\BatchApi;
 use HubSpot\Client\Crm\Deals\Api\GDPRApi;
 use HubSpot\Client\Crm\Deals\Api\PublicObjectApi;
 use HubSpot\Client\Crm\Deals\Api\SearchApi;
+use HubSpot\Client\Crm\Deals\Configuration;
 use HubSpot\Discovery\Crm\ObjectDiscovery;
 
 /**
@@ -16,4 +17,12 @@ use HubSpot\Discovery\Crm\ObjectDiscovery;
  * @method PublicObjectApi publicObjectApi()
  * @method SearchApi       searchApi()
  */
-class Discovery extends ObjectDiscovery {}
+class Discovery extends ObjectDiscovery
+{
+  public function gdprApi()
+  {
+      $config = $this->config->convertToClientConfig(Configuration::class);
+
+      return new GDPRApi($this->client, $config);
+  }
+}
