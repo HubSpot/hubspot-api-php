5b5eb49a43471abd843be1fd5dde9db0ba90570a
ksvirkou-hubspot <ksvirkou@hubspot.com>
2024-02-28 15:54:17 +0300

    CRM Objects: Update Discoveries.

--- lib/Discovery/Crm/Objects/Meetings/Discovery.php
+++ lib/Discovery/Crm/Objects/Meetings/Discovery.php
@@ -4,6 +4,7 @@ namespace HubSpot\Discovery\Crm\Objects\Meetings;
 
 use HubSpot\Client\Crm\Objects\Meetings\Api\BasicApi;
 use HubSpot\Client\Crm\Objects\Meetings\Api\BatchApi;
+use HubSpot\Client\Crm\Objects\Meetings\Api\GDPRApi;
 use HubSpot\Client\Crm\Objects\Meetings\Api\PublicObjectApi;
 use HubSpot\Client\Crm\Objects\Meetings\Api\SearchApi;
 use HubSpot\Discovery\DiscoveryBase;
@@ -11,7 +12,16 @@ use HubSpot\Discovery\DiscoveryBase;
 /**
  * @method BasicApi        basicApi()
  * @method BatchApi        batchApi()
+ * @method GDPRApi         gdprApi()
  * @method PublicObjectApi publicObjectApi()
  * @method SearchApi       searchApi()
  */
-class Discovery extends DiscoveryBase {}
+class Discovery extends DiscoveryBase
+{
+    public function gdprApi()
+    {
+        $config = $this->config->convertToClientConfig(Configuration::class);
+
+        return new GDPRApi($this->client, $config);
+    }
+}
--- lib/Discovery/Crm/Objects/Notes/Discovery.php
+++ lib/Discovery/Crm/Objects/Notes/Discovery.php
@@ -4,14 +4,25 @@ namespace HubSpot\Discovery\Crm\Objects\Notes;
 
 use HubSpot\Client\Crm\Objects\Notes\Api\BasicApi;
 use HubSpot\Client\Crm\Objects\Notes\Api\BatchApi;
+use HubSpot\Client\Crm\Objects\Notes\Api\GDPRApi;
 use HubSpot\Client\Crm\Objects\Notes\Api\PublicObjectApi;
 use HubSpot\Client\Crm\Objects\Notes\Api\SearchApi;
+use HubSpot\Client\Crm\Objects\Notes\Configuration;
 use HubSpot\Discovery\DiscoveryBase;
 
 /**
  * @method BasicApi        basicApi()
  * @method BatchApi        batchApi()
+ * @method GDPRApi         gdprApi()
  * @method PublicObjectApi publicObjectApi()
  * @method SearchApi       searchApi()
  */
-class Discovery extends DiscoveryBase {}
+class Discovery extends DiscoveryBase
+{
+    public function gdprApi()
+    {
+        $config = $this->config->convertToClientConfig(Configuration::class);
+
+        return new GDPRApi($this->client, $config);
+    }
+}
