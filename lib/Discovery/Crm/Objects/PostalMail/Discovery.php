90c7e34c5b7bf3c4d2a15c6904f3e2e43e6f5f39
ksvirkou-hubspot <ksvirkou@hubspot.com>
2024-10-24 11:07:23 +0300

    Update CRM PS, Tasks and Taxes discoveries

--- lib/Discovery/Crm/Objects/PostalMail/Discovery.php
+++ lib/Discovery/Crm/Objects/PostalMail/Discovery.php
@@ -4,25 +4,12 @@ namespace HubSpot\Discovery\Crm\Objects\PostalMail;
 
 use HubSpot\Client\Crm\Objects\PostalMail\Api\BasicApi;
 use HubSpot\Client\Crm\Objects\PostalMail\Api\BatchApi;
-use HubSpot\Client\Crm\Objects\PostalMail\Api\GDPRApi;
-use HubSpot\Client\Crm\Objects\PostalMail\Api\PublicObjectApi;
 use HubSpot\Client\Crm\Objects\PostalMail\Api\SearchApi;
-use HubSpot\Client\Crm\Objects\PostalMail\Configuration;
 use HubSpot\Discovery\DiscoveryBase;
 
 /**
  * @method BasicApi        basicApi()
  * @method BatchApi        batchApi()
- * @method GDPRApi         gdprApi()
- * @method PublicObjectApi publicObjectApi()
  * @method SearchApi       searchApi()
  */
-class Discovery extends DiscoveryBase
-{
-    public function gdprApi()
-    {
-        $config = $this->config->convertToClientConfig(Configuration::class);
-
-        return new GDPRApi($this->client, $config);
-    }
-}
+class Discovery extends DiscoveryBase {}
