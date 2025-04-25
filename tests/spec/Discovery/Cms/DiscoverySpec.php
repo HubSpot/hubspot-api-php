c451bdccc1c368dc2aa870421a3e400a53be59ac
ksvirkou-hubspot <ksvirkou@hubspot.com>
2025-04-25 12:19:33 +0300

    Remove CMS Perfomance API client

--- tests/spec/Discovery/Cms/DiscoverySpec.php
+++ tests/spec/Discovery/Cms/DiscoverySpec.php
@@ -26,7 +26,6 @@ class DiscoverySpec extends ObjectBehavior
         $this->domains()->shouldHaveType(\HubSpot\Discovery\Cms\Domains\Discovery::class);
         $this->hubdb()->shouldHaveType(\HubSpot\Discovery\Cms\Hubdb\Discovery::class);
         $this->pages()->shouldHaveType(\HubSpot\Discovery\Cms\Pages\Discovery::class);
-        $this->performance()->shouldHaveType(\HubSpot\Discovery\Cms\Performance\Discovery::class);
         $this->siteSearch()->shouldHaveType(\HubSpot\Discovery\Cms\SiteSearch\Discovery::class);
         $this->sourceCode()->shouldHaveType(\HubSpot\Discovery\Cms\SourceCode\Discovery::class);
         $this->urlRedirects()->shouldHaveType(\HubSpot\Discovery\Cms\UrlRedirects\Discovery::class);
