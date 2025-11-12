b7065761bccb1ac3d897d05e0505e4c26beb0e11
ksvirkou-hubspot <ksvirkou@hubspot.com>
2025-11-12 15:05:19 +0300

    Fix PHP 8.4 deprecation: Implicitly marking parameter as nullable in CRM Objects

--- codegen/Crm/Objects/Model/CollectionResponseAssociatedId.php
+++ codegen/Crm/Objects/Model/CollectionResponseAssociatedId.php
@@ -248,7 +248,7 @@ class CollectionResponseAssociatedId implements ModelInterface, ArrayAccess, \Js
      * @param mixed[] $data Associated array of property values
      *                      initializing the model
      */
-    public function __construct(array $data = null)
+    public function __construct(?array $data = null)
     {
         $this->setIfExists('paging', $data ?? [], null);
         $this->setIfExists('results', $data ?? [], null);
--- codegen/Crm/Objects/Model/SimplePublicObjectInput.php
+++ codegen/Crm/Objects/Model/SimplePublicObjectInput.php
@@ -242,7 +242,7 @@ class SimplePublicObjectInput implements ModelInterface, ArrayAccess, \JsonSeria
      * @param mixed[] $data Associated array of property values
      *                      initializing the model
      */
-    public function __construct(array $data = null)
+    public function __construct(?array $data = null)
     {
         $this->setIfExists('properties', $data ?? [], null);
     }
