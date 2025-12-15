96dbe3cef1279c439edcb6b2724b3b6756a9c1df
ksvirkou-hubspot <ksvirkou@hubspot.com>
2025-12-15 16:40:49 +0300

    Update Object Type class

--- lib/Crm/ObjectType.php
+++ lib/Crm/ObjectType.php
@@ -4,11 +4,27 @@ namespace HubSpot\Crm;
 
 class ObjectType
 {
-    public const CONTACTS = 'contacts';
     public const COMPANIES = 'companies';
+    public const CONTACTS = 'contacts';
     public const DEALS = 'deals';
-    public const LINE_ITEMS = 'line_items';
-    public const PRODUCTS = 'products';
     public const TICKETS = 'tickets';
+    public const APPOINTMENTS = 'appointments';
+    public const CALLS = 'calls';
+    public const COMMUNICATIONS = 'communications';
+    public const COURSES = 'courses';
+    public const EMAILS = 'emails';
+    public const FEEDBACK_SUBMISSIONS = 'feedback_submissions';
+    public const INVOICES = 'invoices';
+    public const LEADS = 'leads';
+    public const LINE_ITEMS = 'line_items';
+    public const MEETINGS = 'meetings';
+    public const NOTES = 'notes';
+    public const ORDERS = 'order';
+    public const PAYMENTS = 'commerce_payments';
+    public const POSTAL_MAIL = 'postal_mail';
+    public const PRODUCTS = 'products';
     public const QUOTES = 'quotes';
+    public const SUBSCRIPTIONS = 'subscriptions';
+    public const TASKS = 'tasks';
+    public const USERS = 'users';
 }
