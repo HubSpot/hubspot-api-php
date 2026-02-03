d771b8c66330fb483eb726aeea4270bd9caddd54
ksvirkou-hubspot <ksvirkou@hubspot.com>
2026-02-03 15:45:50 +0300

    Update CRM Lists discovery

--- tests/spec/Discovery/Crm/Lists/DiscoverySpec.php
+++ tests/spec/Discovery/Crm/Lists/DiscoverySpec.php
@@ -3,9 +3,10 @@
 namespace spec\HubSpot\Discovery\Crm\Lists;
 
 use GuzzleHttp\Client;
-use HubSpot\Client\Crm\Lists\Api\FoldersApi;
+use HubSpot\Client\Crm\Lists\Api\IDMappingApi;
+use HubSpot\Client\Crm\Lists\Api\JoinOrderApi;
+use HubSpot\Client\Crm\Lists\Api\ListManagementApi;
 use HubSpot\Client\Crm\Lists\Api\ListsApi;
-use HubSpot\Client\Crm\Lists\Api\MappingApi;
 use HubSpot\Client\Crm\Lists\Api\MembershipsApi;
 use HubSpot\Config;
 use HubSpot\Discovery\Crm\Lists\Discovery;
@@ -25,9 +26,10 @@ class DiscoverySpec extends ObjectBehavior
 
     public function it_creates_clients()
     {
-        $this->foldersApi()->shouldHaveType(FoldersApi::class);
+        $this->idMappingApi()->shouldHaveType(IDMappingApi::class);
+        $this->joinOrderApi()->shouldHaveType(JoinOrderApi::class);
+        $this->listManagementApi()->shouldHaveType(ListManagementApi::class);
         $this->listsApi()->shouldHaveType(ListsApi::class);
-        $this->mappingApi()->shouldHaveType(MappingApi::class);
         $this->membershipsApi()->shouldHaveType(MembershipsApi::class);
     }
 }
